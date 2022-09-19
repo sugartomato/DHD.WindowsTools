9f1e8f8f6b487c6df087bae9e9457d6946c2ecea
sugartomato <sugartomato@msn.com>
2022-09-20 07:15:22 +0800

    添加gitignor

--- DHD.WindowsTools/obj/Debug/net6.0-windows/App.g.cs
+++ DHD.WindowsTools/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace DHD.WindowsTools {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -66,7 +66,7 @@ namespace DHD.WindowsTools {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             DHD.WindowsTools.App app = new DHD.WindowsTools.App();
             app.InitializeComponent();
--- DHD.WindowsTools/obj/Debug/net6.0-windows/Controls/MoveToList.g.i.cs
+++ DHD.WindowsTools/obj/Debug/net6.0-windows/Controls/MoveToList.g.i.cs
@@ -87,13 +87,13 @@ namespace DHD.WindowsTools.Controls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DHD.WindowsTools;V1.0.0.0;component/controls/movetolist.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DHD.WindowsTools;component/controls/movetolist.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\MoveToList.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -103,7 +103,7 @@ namespace DHD.WindowsTools.Controls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
